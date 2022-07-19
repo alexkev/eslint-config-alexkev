dad350fc4f0f345a05f2337c7c5994d1dfc42897
Alex Matheson <alex.kmatheson@gmail.com>
2022-07-19 12:59:10 -0500

    not needed

--- react-native.js
+++ react-native.js
@@ -1,11 +1,4 @@
 module.exports = {
-  env: {
-    'browser': true,
-    'es2021': true,
-    'es6': true,
-    'jest': true,
-    'node': true,
-  },
   extends: [
     '@react-native-community',
     './.eslintrc.js',
--- reactjs.js
+++ reactjs.js
@@ -1,11 +1,4 @@
 module.exports = {
-  env: {
-    'browser': true,
-    'es2021': true,
-    'es6': true,
-    'jest': true,
-    'node': true,
-  },
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended', 
